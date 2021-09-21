a0b849d643315d285ca1181156c14b357ba93b05
vivekbeniwaljs <76515013+vivekbeniwaljs@users.noreply.github.com>
2021-09-21 16:20:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,88 +1,94 @@
-var backImage,backgr;
-var player, player_running;
-var ground,ground_img;
+var monkey, monkey_running;
+var banana, bananaImage, obstacle, obstacleImage;
+var FoodGroup, obstacleGroup;
+var score;
+var survivalTime = 0;
 
-var END =0;
-var PLAY =1;
-var gameState = PLAY;
+function preload() {
+  monkey_running = loadAnimation(
+    "sprite_0.png",
+    "sprite_1.png",
+    "sprite_2.png",
+    "sprite_3.png",
+    "sprite_4.png",
+    "sprite_5.png",
+    "sprite_6.png",
+    "sprite_7.png",
+    "sprite_8.png"
+  );
 
-function preload(){
-  backImage=loadImage("jungle.jpg");
-  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
-
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
 }
 
 function setup() {
-  createCanvas(800,400);
-  
-  backgr=createSprite(0,0,800,400);
-  backgr.addImage(backImage);
-  backgr.scale=1.5;
-  backgr.x=backgr.width/2;
-  backgr.velocityX=-4;
-  
-  player = createSprite(100,340,20,50);
-  player.addAnimation("Running",player_running);
-  player.scale = 0.1;
-  
-  ground = createSprite(400,350,800,10);
-  ground.x=ground.width/2;
-  ground.visible=false;
-  
+  createCanvas(600, 600);
+
+  monkey = createSprite(100, 500, 20, 20);
+  monkey.addAnimation("monkey_running", monkey_running);
+  monkey.scale = 0.2;
+
+  ground = createSprite(100, 570, 550, 20);
+  ground.x = ground.width / 2;
+
+  obstacleGroup = createGroup();
+  bananaGroup = createGroup();
+
+  score = 0;
 }
 
-function draw() { 
-  background(0);
-
-  if(gameState===PLAY){
-  
-    if(obstaclesGroup.isTouching(player)){
-      gamestate = END
-    }
-   else if(gameState === END){
-     backgr.velocityX = 0
-     player.visible = false 
-      
-     FoodGroup.destroyEach()
-     obstaclesGroup.destroyEach()
-
-     textSize(30)
-     fill(255)
-     text("Game Over!", 300,220 )
-   }
-
-  if(backgr.x<100){
-    backgr.x=backgr.width/2;    
+function draw() {
+  background("green");
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
   }
-  
-    if(keyDown("space") ) {
-      player.velocityY = -12;
-    }
-    player.velocityY = player.velocityY + 0.8;
-  
-    player.collide(ground);
+  spawnObstacles();
+  spawnBanana();
 
+  if (keyDown("space") && monkey.y >= 300) {
+    monkey.velocityY = -12;
   }
+  if (bananaGroup.isTouching(monkey)) {
+    bananaGroup.destroyEach();
+    score = score + 1;
+  }
+  monkey.velocityY = monkey.velocityY + 0.8;
+
+  monkey.collide(ground);
 
   drawSprites();
+  fill("white");
+  text("Score:" + score, 500, 50);
+
+  fill("black");
+  textSize(20);
+  survivalTime = Math.ceil(frameCount / frameRate());
+  text("Survival Time:" + survivalTime, 100, 50);
 }
+function spawnObstacles() {
+  if (frameCount % 150 === 0) {
+    var obstacle = createSprite(500, 530, 20, 20);
+    obstacle.addImage("o1",obstacleImage);
+    obstacle.velocityX = -6;
+    obstacle.scale = 0.25;
+    obstacle.lifetime = 500;
 
-
-function spawnFood(){
-  if(frameCount%80 === 0 ){
-    var banana = createSprite(600, 250, 40 , 10 )
-    banana.y = random(120,200)
-    banana.addImage(bananaImage)
-    banana.scale = 0.05
-    banana.velocityX = -4
-
-    banana.lifetime = 300
-    player.depth = banana.depth + 1 
-    FoodGroup.add(banana)
+    obstacleGroup.add(obstacle);
   }
-  if(FoodGroup.isTouching(player)){
-    FoodGroup.destroyEach()
-    score = score + 2
-    player.scale += +0.1
+}
+function spawnBanana() {
+  if (frameCount % 160 === 0) {
+    banana = createSprite(600, 100, 40, 10);
+    banana.y = Math.round(random(250, 300));
+    banana.addImage("b1",bananaImage);
+    banana.scale = 0.2;
+    banana.velocityX = -3;
+
+    monkey.lifetime = 500;
+
+    banana.depth = monkey.depth;
+    monkey.depth = monkey.depth + 1;
+
+    bananaGroup.add(banana);
   }
 }
